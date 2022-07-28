6d6351d8da965756af047c5fd57f873d704523d0
Atalyst <869439290@qq.com>
2022-07-29 05:52:56 +0800

    server query 基本功能已实现

--- src/main/java/org/atalyst/Ibox.java
+++ src/main/java/org/atalyst/Ibox.java
@@ -1,32 +1,35 @@
 package org.atalyst;
 
-import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.console.data.AutoSavePluginConfig;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+import net.mamoe.mirai.event.EventChannel;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import net.mamoe.mirai.event.Listener;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.MessageChain;
+import org.atalyst.config.MyConfig;
+import org.atalyst.event.MyMessageEvent;
+
 
 public final class Ibox extends JavaPlugin {
+    //插件信息
     public static final Ibox INSTANCE = new Ibox();
 
-        private Ibox() {
-                        super(new JvmPluginDescriptionBuilder("org.atalyst.ibox", "0.1")
-            .name("IboxServerPlugin")
-            .author("Atalyst，Feu d'artifice")
-                    .build());    }
+    private Ibox() {
+        super(new JvmPluginDescriptionBuilder("org.atalyst.ibox", "0.1")
+                .name("IboxServerPlugin")
+                .author("Atalyst，Feu_d'artifice")
+                .build());
+    }
 
     @Override
     public void onEnable() {
-        AutoSavePluginConfig("test");
-
+        //加载config
+        reloadPluginConfig(MyConfig.INSTANCE);
         getLogger().info("IboxPlugins已加载!");
-        // 创建监听
-        Listener listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, a->{
-            MessageChain chain=a.getMessage(); // 可获取到消息内容等, 详细查阅 `GroupMessageEvent`
-            a.getSubject().sendMessage("Hello!"); // 回复消息
-        });
+
+        //群消息事件监听
+        EventChannel<GroupMessageEvent> messageEvent =
+                GlobalEventChannel.INSTANCE.filterIsInstance(GroupMessageEvent.class);
+        //群消息事件
+        messageEvent.subscribeAlways(GroupMessageEvent.class, MyMessageEvent.INSTANCE::messageEventHandle);
     }
 }
