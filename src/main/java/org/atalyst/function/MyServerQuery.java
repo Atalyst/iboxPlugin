10b6b3aa3057284b94d0a8932377e8c72f5626c2
Atalyst <869439290@qq.com>
2022-08-01 23:12:11 +0800

    server query 完成

--- src/main/java/org/atalyst/function/MyServerQuery.java
+++ src/main/java/org/atalyst/function/MyServerQuery.java
@@ -61,20 +61,39 @@ public class MyServerQuery {
 
     private MessageChain sendServerMessage(SourceServer server,GroupMessageEvent event) {
         StringBuilder sb = new StringBuilder("\n");
+        StringBuilder ab = new StringBuilder(server.getIpAddresses().toString());
+        String address,passwordProtected,vac;
+
+        String[] temp;
         MessageChain sendChain = null;
-        
+        address = ab.deleteCharAt(0).toString();                                                                          //预处理字符串
+        temp = address.split("/");
         try {
+            if(server.getServerInfo().get("passwordProtected").toString().equals("true")){          //*
+                passwordProtected = "开启";
+            }else {
+                passwordProtected = "无";
+            }
+                                                                                                    //*
+            if(server.getServerInfo().get("secure").toString().equals("true")){
+                vac = "安全";
+            }else {
+                vac = "关闭";
+            }                                                                                       //预处理字符串
+
             Set<Entry<String, SteamPlayer>> serverSet = server.getPlayers().entrySet();
             if (serverIsEmpty(server,event)) {
                 sb.append("暂无玩家……");
             } else {
                 for (Entry<String, SteamPlayer> sf : serverSet) {          //    玩家信息查询
-                    if (!(sf.getValue().getName().length() == 0) && !(sf.getValue().getScore() == 0)) {
+                    if (!(sf.getValue().getName().length() == 0 && sf.getValue().getScore() == 0)) {
                         sb.append("玩家：");
                         sb.append(sf.getValue().getName());
                         sb.append("\t得分：");
                         sb.append(sf.getValue().getScore());
                         sb.append("\n");
+                    }else if(sf.getValue().getName().length() == 0 && sf.getValue().getScore() == 0){
+                        sb.append("玩家：载入中……\n");
                     }
                 }
             }
@@ -84,20 +103,18 @@ public class MyServerQuery {
                     .append("\n名称：")
                     .append(server.getServerInfo().get("serverName").toString())
                     .append("\n地址：")
-                    .append(server.getIpAddresses().toString()).append(":").append(server.getServerInfo().get("serverPort").toString())
+                    .append(temp[0]).append(":").append(server.getServerInfo().get("serverPort").toString())
                     .append("\n地图：")
                     .append(server.getServerInfo().get("mapName").toString())
                     .append("\n游戏：")
-                    .append(server.getServerInfo().get("gameDir").toString())
-                    .append("\n标签：")
-                    .append(server.getServerInfo().get("serverTags").toString())
+                    .append(server.getServerInfo().get("gameDir").toString()).append(" - ").append(server.getServerInfo().get("gameDescription").toString())
                     .append("\n人数：")
                     .append(server.getServerInfo().get("numberOfPlayers").toString()).append("|").append(server.getServerInfo().get("maxPlayers").toString()).append(" (")
                     .append(server.getServerInfo().get("numberOfBots").toString()).append(" Bot)")
                     .append("\n密码保护：")
-                    .append(server.getServerInfo().get("passwordProtected").toString())
+                    .append(passwordProtected)
                     .append("\nVAC状态：")
-                    .append(server.getServerInfo().get("secure").toString())
+                    .append(vac)
                     .append("\n——————————————")
                     .append(sb)
                     .build();
